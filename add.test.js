791b9ac42a608dac9e0282a62919ce4a5c7a9917
Dino Anastasopoulos <dinoanasta@gmail.com>
2020-03-25 15:45:34 +0200

    added c

--- add.test.js
+++ add.test.js
@@ -1,7 +1,7 @@
 const add = require('./add')
 
-test('adds 1 + 2 to equal 3', additionTest);
+test('adds 1 + 2 + 3 to equal 6', additionTest);
 
 function additionTest(){
-	expect(add(1,2)).toBe(3);
+	expect(add(1,2,3)).toBe(6);
 }
